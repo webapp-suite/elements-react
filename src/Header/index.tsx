b829b75af7ed648c755bbeb5c452a714b0f34368
Kimi Gao <kimi.gao@outlook.com>
2021-02-23 20:09:54 +0800

    add className prop in header

--- src/Header/index.tsx
+++ src/Header/index.tsx
@@ -1,6 +1,8 @@
 import '@tradeshift/elements.header';
 import { adapt } from 'webcomponents-in-react';
 
-const Header = adapt("ts-header");
+const Header = adapt("ts-header", {
+    className: 'class'
+});
 
 export default Header;
