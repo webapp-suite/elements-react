55dc8bb49b9c8977775f648307f5d32431b01ded
Kimi Gao <kimi.gao@outlook.com>
2021-03-02 19:52:20 +0800

    add onClick mapping for Button

--- src/Button/index.tsx
+++ src/Button/index.tsx
@@ -2,7 +2,8 @@ import '@tradeshift/elements.button';
 import { adapt } from 'webcomponents-in-react';
 
 const Button = adapt("ts-button", {
-    className: 'class'
+    className: 'class',
+    onClick: 'button-click'
 });
 
 export default Button;
