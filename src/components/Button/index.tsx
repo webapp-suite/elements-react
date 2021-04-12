6aab2f477e71c0654cd535c05637a1b67c032572
Kimi Gao <kimi.gao@outlook.com>
2021-04-12 20:45:49 +0800

    refactor button

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,9 +1,39 @@
 import '@webapp-suite/elements.button';
-import { adapt } from 'webcomponents-in-react';
+import React, { MouseEventHandler } from "react";
+import { convertToWebComponent, WebComponentPropTypes } from "../../common/convertToWebComponent";
 
-const Button = adapt("ts-button", {
-    className: 'class',
-    onClick: 'button-click'
-});
+export interface ButtonPropTypes extends WebComponentPropTypes {
+    type?: "primary" | "secondary" | "text" | "accept" | "warning" | "danger";
+    size?: "macro" | "micro";
+    dir?: "rtl" | "ltr";
+    icon?: string;
+    busy?: boolean;
+    disabled?: boolean;
+    grouped?: boolean;
+    focused?: boolean;
+    onClick?: MouseEventHandler<HTMLElement>;
+}
+
+const Button: React.FC<ButtonPropTypes> = convertToWebComponent<ButtonPropTypes>(
+    "ts-button",
+    ["type", "size", "dir", "icon"],
+    ["busy", "disabled", "grouped", "focused"],
+    ["default"],
+    ["click"],
+    {
+        click: "button-click"
+    }
+);
+
+Button.displayName = "Button";
+
+Button.defaultProps = {
+    type: "secondary",
+    busy: false,
+    dir: "ltr",
+    disabled: false,
+    grouped: false,
+    focused: false,
+};
 
 export default Button;
