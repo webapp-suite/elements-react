07a1455c37a8a1f43bbe04f057fa7ea9710149f7
Kimi Gao <kimi.gao@outlook.com>
2021-04-12 19:56:35 +0800

    refactor modal and add more props

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -1,15 +1,39 @@
 import '@webapp-suite/elements.modal';
-import { adapt } from 'webcomponents-in-react';
+import React from "react";
+import { convertToWebComponent, WebComponentPropTypes } from "../../common/convertToWebComponent";
 
-const Modal = adapt("ts-modal", {
-    visible: "data-visible",
-    title: "data-title",
-    size: "data-size",
-    onClose: "close",
-    onOpen: "open",
-    noPadding: "no-padding",
-    hideHeader: "hide-header",
-    className: 'class'
-});
+export interface ModalPropTypes extends WebComponentPropTypes {
+    dataVisible: boolean;
+    dataTitle?: string;
+    dataDir?: string;
+    dataSize?: "small" | "medium" | "large";
+    noCloseOnEscKey?: boolean;
+    hideHeader?: boolean;
+    noPadding?: boolean;
+    onOpened?: (event: CustomEvent) => void;
+    onClosed?: (event: CustomEvent) => void;
+    onOpen?: (event: CustomEvent) => void;
+    onClose?: (event: CustomEvent) => void;
+}
+
+const Modal: React.FC<ModalPropTypes> = convertToWebComponent<ModalPropTypes>(
+    "ts-modal",
+    ["dataTitle", "dataDir", "dataSize"],
+    ["dataVisible", "noCloseOnEscKey", "hideHeader", "noPadding"],
+    ["note", "main", "footer"],
+    ["opened", "closed", "open", "close"]
+);
+
+Modal.displayName = "Modal";
+
+Modal.defaultProps = {
+    dataVisible: false,
+    dataTitle: "",
+    dataDir: "ltr",
+    dataSize: "large",
+    noCloseOnEscKey: false,
+    hideHeader: false,
+    noPadding: false
+};
 
 export default Modal;
