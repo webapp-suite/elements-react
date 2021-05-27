92143f4d135b9cae2b221d3ac791fd9b04f1ac04
Kimi Gao <kimi.gao@outlook.com>
2021-05-27 23:57:01 +0800

    update dialog storybook

--- src/components/Dialog/__stories__/index.stories.tsx
+++ src/components/Dialog/__stories__/index.stories.tsx
@@ -1,15 +1,53 @@
 import { Meta } from "@storybook/react";
 
-import Header from "../../Header";
+import Dialog from "../../Dialog";
 import Button from "../../Button";
 
 export default {
     title: "Layer/Dialog",
-    component: Header,
+    component: Dialog,
 } as Meta;
 
-export const Basic = () => <Header title="My Application" />;
+export const Basic = () => (
+    <Button
+        onClick={() => {
+            Dialog.info({
+                text: "This is info hint text !",
+            });
+        }}
+    >
+        info
+    </Button>
+);
 
-export const Color = () => <Header title="My Application" color="blue" />;
-
-export const Action = () => <Header title="My Application" color="blue"><Button>Button</Button></Header>;
+export const Type = () => (
+    <>
+        <Button
+            onClick={() => {
+                Dialog.info({
+                    text: "This is info hint text !",
+                });
+            }}
+        >
+            info
+        </Button>
+        <Button
+            onClick={() => {
+                Dialog.warning({
+                    text: "This is warning hint text !",
+                });
+            }}
+        >
+            warning
+        </Button>
+        <Button
+            onClick={() => {
+                Dialog.danger({
+                    text: "This is danger hint text !",
+                });
+            }}
+        >
+            danger
+        </Button>
+    </>
+);
