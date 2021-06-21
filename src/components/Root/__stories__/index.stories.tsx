6dc437f8cae618460a48f3f72f25c3e104482cb3
Kimi Gao <kimi.gao@outlook.com>
2021-06-22 00:10:12 +0800

    update Root story

--- src/components/Root/__stories__/index.stories.tsx
+++ src/components/Root/__stories__/index.stories.tsx
@@ -1,15 +1,10 @@
 import { Meta } from "@storybook/react";
 
-import Header from "../../Header";
-import Button from "../../Button";
+import Root from "..";
 
 export default {
     title: "Layout/Root",
-    component: Header,
+    component: Root,
 } as Meta;
 
-export const Basic = () => <Header title="My Application" />;
-
-export const Color = () => <Header title="My Application" color="blue" />;
-
-export const Action = () => <Header title="My Application" color="blue"><Button>Button</Button></Header>;
+export const Basic = () => <Root>Root</Root>;
