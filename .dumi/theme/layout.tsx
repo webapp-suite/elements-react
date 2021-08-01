d4725ab99c5d31d0766fb666df8a1ae90d80d1a0
Kimi Gao <kimi.gao@outlook.com>
2021-08-02 02:20:54 +0800

    fix(theme): scrollbar issue in layout

--- .dumi/theme/layout.tsx
+++ .dumi/theme/layout.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Layout from 'dumi-theme-default/src/layout';
 import { Root } from '@/index';
+import './style/layout.less';
 
 // @ts-ignore
 export default (props) => (
