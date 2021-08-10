ea67cca992e7ad75b7983077b0e0ca4677b642ce
Kimi Gao <kimi.gao@outlook.com>
2021-08-10 23:56:33 +0800

    format imports in theme layout

--- .dumi/theme/layout.tsx
+++ .dumi/theme/layout.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import Layout from 'dumi-theme-default/src/layout';
 import { Root } from '@/index';
+import Layout from 'dumi-theme-default/src/layout';
+import React from 'react';
 import ScrollToTop from './ScrollToTop';
 import './style/layout.less';
 
